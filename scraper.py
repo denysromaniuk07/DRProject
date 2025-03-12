d48a8d9765d112a47143ccdac21a32eb84e064aa
Denys Romaniuk <237703@student.uek.krakow.pl>
2025-03-12 13:39:03 +0100

    Stage 2 completed

--- scraper.py
+++ scraper.py
@@ -11,30 +11,34 @@ HEADERS = {
 
 response = requests.get(URL, headers=HEADERS)
 if response.status_code != 200:
-    print("Помилка завантаження сторінки")
+    print("Error loading the page")
     exit()
 
 soup = BeautifulSoup(response.text, "html.parser")
 
-first_review = soup.select_one(".js_product-review")
-if not first_review:
+reviews = soup.select(".js_product-review")
+if not reviews:
     print("No reviews found")
     exit()
 
-review_data = {
-    "opinion_id": first_review.get("data-entry-id", ""),
-    "author": first_review.select_one(".user-post__author-name").text.strip() if first_review.select_one(".user-post__author-name") else "",
-    "score": first_review.select_one(".user-post__score-count").text.strip() if first_review.select_one(".user-post__score-count") else "",
-    "content": first_review.select_one(".user-post__text").text.strip() if first_review.select_one(".user-post__text") else "",
-    "advantages": [li.text.strip() for li in first_review.select(".review-feature__title--positives ~ .review-feature__item")] if first_review.select(".review-feature__title--positives ~ .review-feature__item") else [],
-    "disadvantages": [li.text.strip() for li in first_review.select(".review-feature__title--negatives ~ .review-feature__item")] if first_review.select(".review-feature__title--negatives ~ .review-feature__item") else [],
-    "helpful": first_review.select_one(".vote-yes .js_product-review-vote")['data-total-vote'] if first_review.select_one(".vote-yes .js_product-review-vote") else "0",
-    "unhelpful": first_review.select_one(".vote-no .js_product-review-vote")['data-total-vote'] if first_review.select_one(".vote-no .js_product-review-vote") else "0",
-    "publish_date": first_review.select_one(".user-post__published > time:nth-of-type(1)")['datetime'] if first_review.select_one(".user-post__published > time:nth-of-type(1)") else "",
-    "purchase_date": first_review.select_one(".user-post__published > time:nth-of-type(2)")['datetime'] if first_review.select_one(".user-post__published > time:nth-of-type(2)") else ""
-}
+reviews_data = []
+for review in reviews:
+    review_data = {
+        "opinion_id": review.get("data-entry-id", ""),
+        "author": review.select_one(".user-post__author-name").text.strip() if review.select_one(".user-post__author-name") else "",
+        "score": review.select_one(".user-post__score-count").text.strip() if review.select_one(".user-post__score-count") else "",
+        "content": review.select_one(".user-post__text").text.strip() if review.select_one(".user-post__text") else "",
+        "advantages": [li.text.strip() for li in review.select(".review-feature__title--positives ~ .review-feature__item")] if review.select(".review-feature__title--positives ~ .review-feature__item") else [],
+        "disadvantages": [li.text.strip() for li in review.select(".review-feature__title--negatives ~ .review-feature__item")] if review.select(".review-feature__title--negatives ~ .review-feature__item") else [],
+        "helpful": review.select_one(".vote-yes .js_product-review-vote")['data-total-vote'] if review.select_one(".vote-yes .js_product-review-vote") else "0",
+        "unhelpful": review.select_one(".vote-no .js_product-review-vote")['data-total-vote'] if review.select_one(".vote-no .js_product-review-vote") else "0",
+        "publish_date": review.select_one(".user-post__published > time:nth-of-type(1)")['datetime'] if review.select_one(".user-post__published > time:nth-of-type(1)") else "",
+        "purchase_date": review.select_one(".user-post__published > time:nth-of-type(2)")['datetime'] if review.select_one(".user-post__published > time:nth-of-type(2)") else ""
+    }
+    reviews_data.append(review_data)
 
-with open("review.json", "w", encoding="utf-8") as f:
-    json.dump(review_data, f, ensure_ascii=False, indent=4)
 
-print("Firts review saved in 'review.json'")
+with open("reviews.json", "w", encoding="utf-8") as f:
+    json.dump(reviews_data, f, ensure_ascii=False, indent=4)
+
+print("All reviews from the first page have been saved to 'reviews.json'")
